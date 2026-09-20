f02794b41a1cab9a21b8b9a81853185070c83e03
ljq <771373073@qq.com>
2026-09-20 13:15:55 +0800

    chore: prepare dashboard release v0.2.0

--- go/main.go
+++ go/main.go
@@ -936,7 +936,7 @@ func pluginRegistration() registration {
 		SchemaVersion: pluginabi.SchemaVersion,
 		Metadata: pluginapi.Metadata{
 			Name:             "codex-turn-state",
-			Version:          "0.1.0",
+			Version:          "0.2.0",
 			Author:           "arden-aaai",
 			GitHubRepository: "https://github.com/arden-aaai/cpa-plugin-codex-turn-state",
 			ConfigFields: []pluginapi.ConfigField{
